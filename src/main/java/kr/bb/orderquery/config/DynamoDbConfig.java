46d4482578026062e7ccc57d9a9c45aed3f29a1b
qwerty1434 <qwerty1434@naver.com>
2023-12-11 19:16:56 +0900

    :wrench: DynamoConfig 작성 #LF1-627

---
+++ src/main/java/kr/bb/orderquery/config/DynamoDbConfig.java
@@ -0,0 +1,68 @@
+package kr.bb.orderquery.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverter;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.TimeZone;
+
+
+@Configuration
+@EnableDynamoDBRepositories(basePackages = "kr.bb.orderquery.repository")
+public class DynamoDbConfig {
+    @Value("${aws.dynamodb.endpoint}")
+    private String amazonDynamoDbEndpoint;
+
+    @Value("${cloud.aws.region.static}")
+    private String amazonDynamoDbRegion;
+
+    @Value("${cloud.aws.credentials.ACCESS_KEY_ID}")
+    private String amazonAwsAccessKey;
+
+    @Value("${cloud.aws.credentials.SECRET_ACCESS_KEY}")
+    private String amazonAwsSecretKey;
+
+    @Primary
+    @Bean
+    public DynamoDBMapper dynamoDbMapper(AmazonDynamoDB amazonDynamoDb) {
+        return new DynamoDBMapper(amazonDynamoDb, DynamoDBMapperConfig.DEFAULT);
+    }
+
+    @Bean(name = "amazonDynamoDB")
+    public AmazonDynamoDB amazonDynamoDb() {
+        AWSStaticCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(
+                new BasicAWSCredentials(amazonAwsAccessKey, amazonAwsSecretKey));
+        AwsClientBuilder.EndpointConfiguration endpointConfiguration =
+                new AwsClientBuilder.EndpointConfiguration(amazonDynamoDbEndpoint, amazonDynamoDbRegion);
+
+        return AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(credentialsProvider)
+                .withEndpointConfiguration(endpointConfiguration).build();
+    }
+
+    // LocalDateTime Converter
+    public static class LocalDateTimeConverter implements DynamoDBTypeConverter<Date, LocalDateTime> {
+        @Override
+        public Date convert(LocalDateTime source) {
+            return Date.from(source.toInstant(ZoneOffset.UTC));
+        }
+
+        @Override
+        public LocalDateTime unconvert(Date source) {
+            return source.toInstant().atZone(TimeZone.getDefault().toZoneId()).toLocalDateTime();
+        }
+    }
+}
