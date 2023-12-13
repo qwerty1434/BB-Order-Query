e300466f7741e4135e31c3a9b2cabaab0af454cb
qwerty1434 <qwerty1434@naver.com>
2023-12-13 19:45:48 +0900

    :pencil2: feign  요청 수정

--- src/main/java/kr/bb/orderquery/client/StoreFeignClient.java
+++ src/main/java/kr/bb/orderquery/client/StoreFeignClient.java
@@ -3,12 +3,14 @@ package kr.bb.orderquery.client;
 import kr.bb.orderquery.client.dto.StoreInfoDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "store-service")
 public interface StoreFeignClient {
     @GetMapping
     String getStoreAddress(Long storeId);
 
-    @GetMapping
-    StoreInfoDto getStoreInfo(Long storeId);
+    @GetMapping("/stores/{storeId}/info")
+    StoreInfoDto getStoreInfo(@PathVariable Long storeId);
 }
