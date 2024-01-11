36f27122aa52d1e07f84021d79bf691ea777786f
qwerty1434 <qwerty1434@naver.com>
2024-01-11 17:49:28 +0900

    :pencil2: pickup 정보에 productId 추가

--- src/main/java/kr/bb/orderquery/domain/pickup/handler/PickupCreator.java
+++ src/main/java/kr/bb/orderquery/domain/pickup/handler/PickupCreator.java
@@ -43,6 +43,7 @@ public class PickupCreator {
                 .reservationStatus(pickupCreateDto.getReservationStatus())
                 .reviewStatus(pickupCreateDto.getReviewStatus())
                 .cardStatus(pickupCreateDto.getCardStatus())
+                .productId(pickupCreateDto.getProductId())
                 .build();
 
         return pickupRepository.save(pickup);
